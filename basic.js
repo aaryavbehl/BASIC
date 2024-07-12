15074c57a06924486df1fadbcf74a7f54fc29a89
Aaryav Behl <aaryavbehl47@gmail.com>
2024-07-13 01:32:36 +0530

    modified:   basic.js

--- basic.js
+++ basic.js
@@ -313,3 +313,214 @@ this.basic = (function() {
       var s = name.charAt(name.length - 1);
       return s === '$' ? 'string' : s === '%' ? 'int' : 'float';
     }
+
+    peek_table = {
+
+      0x0020: function() { return env.tty.textWindow ? env.tty.textWindow.left : 0; },
+      0x0021: function() { return env.tty.textWindow ? env.tty.textWindow.width : 80; },
+      0x0022: function() { return env.tty.textWindow ? env.tty.textWindow.top : 0; },
+      0x0023: function() { return env.tty.textWindow ? env.tty.textWindow.top + env.tty.textWindow.height : 24; },
+      0x0024: function() { return env.tty.getCursorPosition().x; },
+      0x0025: function() { return env.tty.getCursorPosition().y; },
+
+      0x004e: function() { return (Math.random() * 256) & 0xff; },
+      0x004f: function() { return (Math.random() * 256) & 0xff; },
+
+      0x00de: function() { return state.onerr_code; },
+
+      0x00e6: function() { return env.display ? (env.display.hires_plotting_page === 2 ? 64 : 32) : 0; },
+
+      0xC000: function() { return env.tty.getKeyboardRegister ? env.tty.getKeyboardRegister() : 0; },
+      0xC010: function() { return env.tty.clearKeyboardStrobe ? env.tty.clearKeyboardStrobe() : 0; },
+
+      0xC030: function() { return 0; },
+
+      0xC060: function() { return env.tty.getButtonState ? env.tty.getButtonState(3) : 0; },
+      0xC061: function() { return env.tty.getButtonState ? env.tty.getButtonState(0) : 0; },
+      0xC062: function() { return env.tty.getButtonState ? env.tty.getButtonState(1) : 0; },
+      0xC063: function() { return env.tty.getButtonState ? env.tty.getButtonState(2) : 0; },
+
+      0xC01A: function() { return (env.display && !env.display.getState().graphics) * 128; },
+      0xC01B: function() { return (env.display && !env.display.getState().full) * 128; },
+      0xC01C: function() { return (env.display && !env.display.getState().page1) * 128; },
+      0xC01D: function() { return (env.display && !env.display.getState().lores) * 128; },
+      0xC01E: function() { return (env.tty.isAltCharset && env.tty.isAltCharset()) * 128; },
+      0xC01F: function() { return (env.tty.isFirmwareActive && env.tty.isFirmwareActive()) * 128; }
+    };
+
+    poke_table = {
+
+      0x0020: function(v) { if (env.tty.textWindow) { env.tty.textWindow.left = v; } },
+      0x0021: function(v) { if (env.tty.textWindow) { env.tty.textWindow.width = v; } },
+      0x0022: function(v) { if (env.tty.textWindow) {
+        var bottom = env.tty.textWindow.top + env.tty.textWindow.height;
+        env.tty.textWindow.top = v;
+        env.tty.textWindow.height = bottom - env.tty.textWindow.top;
+      } },
+      0x0023: function(v) { if (env.tty.textWindow) { env.tty.textWindow.height = v - env.tty.textWindow.top; } },
+      0x0024: function(v) { env.tty.setCursorPosition(v, void 0); },
+      0x0025: function(v) { env.tty.setCursorPosition(void 0, v); },
+
+      0x00D8: function(v) { if (v < 0x80) { state.onerr_handler = (void 0); } },
+
+      0x00E6: function(v) { if (env.display) { env.display.hires_plotting_page = (v === 64 ? 2 : 1); } },
+
+      0xC010: function() { if (env.tty.clearKeyboardStrobe) { env.tty.clearKeyboardStrobe(); } },
+
+      0xC050: function() { if (env.display) { env.display.setState("graphics", true); } }, 
+      0xC051: function() { if (env.display) { env.display.setState("graphics", false); } }, 
+      0xC052: function() { if (env.display) { env.display.setState("full", true); } }, 
+      0xC053: function() { if (env.display) { env.display.setState("full", false); } }, 
+      0xC054: function() { if (env.display) { env.display.setState("page1", true); } }, 
+      0xC055: function() { if (env.display) { env.display.setState("page1", false); } }, 
+      0xC056: function() { if (env.display) { env.display.setState("lores", true); } }, 
+      0xC057: function() { if (env.display) { env.display.setState("lores", false); } }, 
+
+      0xC030: function() { } 
+    };
+
+    call_table = {
+      0xD683: function() {
+        state.stack = [];
+      },
+      0xF328: function() { 
+        var stack_record = state.stack.pop();
+        if (!{}.hasOwnProperty.call(stack_record, 'resume_stmt_index')) {
+          runtime_error(ERRORS.SYNTAX_ERROR);
+          return;
+        }
+      },
+      0xF3E4: function() { 
+        if (!env.hires) { runtime_error('Hires graphics not supported'); }
+        env.display.setState('graphics', true, 'full', true, 'page1', true, 'lores', false);
+      },
+      0xF3F2: function() { 
+        var hires = env.display.hires_plotting_page === 2 ? env.hires2 : env.hires;
+        if (!hires) { runtime_error('Hires graphics not supported'); }
+        hires.clear();
+      },
+      0xF3F6: function() { 
+        var hires = env.display.hires_plotting_page === 2 ? env.hires2 : env.hires;
+        if (!hires) { runtime_error('Hires graphics not supported'); }
+        hires.clear(hires.color);
+      },
+      0xFBF4: function() { 
+        if (env.tty.cursorRight) { env.tty.cursorRight(); }
+      },
+      0xFC10: function() { 
+        if (env.tty.cursorLeft) { env.tty.cursorLeft(); }
+      },
+      0xFC1A: function() { 
+        if (env.tty.cursorUp) { env.tty.cursorUp(); }
+      },
+      0xFC42: function() {
+        if (env.tty.clearEOS) { env.tty.clearEOS(); }
+      },
+      0xFC66: function() { 
+        if (env.tty.cursorDown) { env.tty.cursorDown(); }
+      },
+      0xFC9C: function() { 
+        if (env.tty.clearEOL) { env.tty.clearEOL(); }
+      },
+      0xFD0C: function() { 
+        throw new BlockingInput(env.tty.readChar, function(_){});
+      },
+      0xFE84: function() { 
+        if (env.tty.setTextStyle) { env.tty.setTextStyle(env.tty.TEXT_STYLE_NORMAL); }
+      },
+      0xFE80: function() { 
+        if (env.tty.setTextStyle) { env.tty.setTextStyle(env.tty.TEXT_STYLE_INVERSE); }
+      }
+    };
+
+    lib = {
+
+      'clear': function CLEAR() {
+        state.clear();
+      },
+
+      'dim': function DIM(name, subscripts) {
+        state.arrays[name].dim(subscripts);
+      },
+
+      'def': function DEF(name, func) {
+        state.functions[name] = func;
+      },
+
+      'goto': function GOTO(line) {
+        throw new GoToLine(line);
+      },
+
+      'on_goto': function ON_GOTO(index /* , ...lines */) {
+        index = Math.floor(index);
+        if (index < 0 || index > 255) {
+          runtime_error(ERRORS.ILLEGAL_QUANTITY);
+        }
+        --index;
+        var lines = Array.prototype.slice.call(arguments, 1);
+
+        if (index >= 0 && index < lines.length) {
+          throw new GoToLine(lines[index]);
+        }
+      },
+
+      'gosub': function GOSUB(line) {
+        state.stack.push({
+          gosub_return: state.stmt_index,
+          line_number: state.line_number
+        });
+        throw new GoToLine(line);
+      },
+    
+      var env,          
+      lib,       
+      funlib,    
+      peek_table,
+      poke_table,
+      call_table;
+
+    'on_gosub': function ON_GOSUB(index /* , ...lines */) {
+      index = Math.floor(index);
+      if (index < 0 || index > 255) {
+        runtime_error(ERRORS.ILLEGAL_QUANTITY);
+      }
+      --index;
+      var lines = Array.prototype.slice.call(arguments, 1);
+      if (index >= 0 && index < lines.length) {
+        state.stack.push({
+          gosub_return: state.stmt_index,
+          line_number: state.line_number
+        });
+        throw new GoToLine(lines[index]);
+      }
+    },
+    'return': function RETURN() {
+        var stack_record;
+        while (state.stack.length) {
+          stack_record = state.stack.pop();
+          if ({}.hasOwnProperty.call(stack_record, 'gosub_return')) {
+            state.stmt_index = stack_record.gosub_return;
+            state.line_number = stack_record.line_number;
+            return;
+          }
+        }
+        runtime_error(ERRORS.RETURN_WITHOUT_GOSUB);
+      },
+      
+      'pop': function POP() {
+        var stack_record = state.stack.pop();
+        if (!{}.hasOwnProperty.call(stack_record, 'gosub_return')) {
+          runtime_error(ERRORS.RETURN_WITHOUT_GOSUB);
+          return;
+        }
+      },
+
+      'for': function FOR(varname, to, step) {
+        state.stack.push({
+          index: varname,
+          to: to,
+          step: step,
+          for_next: state.stmt_index,
+          line_number: state.line_number
+        });
+      },
